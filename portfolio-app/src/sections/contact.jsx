d227884a85b1a1341b679ee2c2a7d0df4189d526
Krishaanth <krish100@my.yorku.ca>
2022-12-21 02:06:22 -0500

    Changed Form to use reactbootstrap

--- portfolio-app/src/sections/contact.jsx
+++ portfolio-app/src/sections/contact.jsx
@@ -1,12 +1,19 @@
 import React, { useRef } from "react";
+import { Form, Button, Container } from "react-bootstrap";
 import emailjs from "@emailjs/browser";
 import "./section.css";
 
 const Contact = (props) => {
   return (
-    <div id="contact" className="section-contact-jumbo">
-      <h1>contact form here i gues?</h1>
-      <ContactUs />
+    <div
+      id="contact"
+      className="section-contact-jumbo"
+      style={{ margin: "50px" }}
+    >
+      <Container>
+        <h1>contact form here i gues?</h1>
+        <ContactUs />
+      </Container>
     </div>
   );
 };
@@ -19,6 +26,7 @@ export const ContactUs = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
+    console.log(e);
     emailjs
       .sendForm(
         process.env.REACT_APP_SERVICE_KEY, //service
@@ -37,14 +45,42 @@ export const ContactUs = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    // <form ref={form} onSubmit={sendEmail}>
+    //   <label>Name</label>
+    //   <input type="text" name="user_name" />
+    //   <label>Email</label>
+    //   <input type="email" name="user_email" />
+    //   <label>Messagesss</label>
+    //   <textarea name="message" />
+    //   <input type="submit" value="Send" />
+    // </form>
+
+    <Form ref={form} onSubmit={sendEmail}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="<Insert Email Here>"
+          name="user_email"
+        />
+        {/* <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text> */}
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Message here blah</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={3}
+          placeholder="<Let me know what's up!>"
+          name="message"
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 };
